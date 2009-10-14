e11f58a993a3695af2213b914a742bf4fc966c1a
Carol <Carol@e2ae85e3-cf20-0410-855b-be2d16f42433>
2009-10-14 17:58:14 +0000

    add top copyright statement
    git-svn-id: svn://tupelo.fcla.edu/daitss2/describe/trunk@2278 e2ae85e3-cf20-0410-855b-be2d16f42433

--- describe.rb
+++ describe.rb
@@ -1,3 +1,19 @@
+# DAITSS Copyright (C) 2009 University of Florida
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation; either version 3 of the License, or
+# (at your option) any later version.
+# 
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+# GNU General Public License for more details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+
 $:.unshift File.join(File.dirname(__FILE__), 'lib')
 
 # describe.rb
@@ -15,7 +31,7 @@ require 'ftools'
 require 'pp'
 require 'net/http'
 
-#load all required JAVA library.
+#load all required JAVA libraries.
 jar_pattern = File.expand_path File.join(File.dirname(__FILE__), 'jars', '*.jar')
 jars = Dir[jar_pattern].join ':'
 Rjb::load jars
