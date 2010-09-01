6a70bccb9f0a25a44fe9ce623b4a2c8eb4071174
Carol <cchou@ufl.edu>
2010-09-01 11:24:24 -0400

    using libxml parsing and erb template to convert jhove output to docmd.

--- lib/format/tiff.rb
+++ lib/format/tiff.rb
@@ -10,31 +10,31 @@ class Tiff < Image
       createDate =  @mix.find_first('mix:ImageCaptureMetadata/mix:GeneralCaptureInformation/mix:dateTimeCreated', NAMESPACES)
       unless (createDate.nil?)
         @fileObject.createDate = createDate.content
-      end
+     end
 
-      createAppName = @mix.find_first('mix:ImageCaptureMetadata/mix:ScannerCapture/mix:ScanningSystemSoftware/mix:scanningSoftwareName', NAMESPACES)
-      unless (createAppName.nil?)
-        @fileObject.createAppName = createAppName.content
-      end
+     createAppName = @mix.find_first('mix:ImageCaptureMetadata/mix:ScannerCapture/mix:ScanningSystemSoftware/mix:scanningSoftwareName', NAMESPACES)
+     unless (createAppName.nil?)
+       @fileObject.createAppName = createAppName.content
+     end
 
-    end
-    # traverse through multiple image bitstreams inside TIFF 
-    nodes = @jhove.find("//jhove:property[jhove:name/text()='NisoImageMetadata']/jhove:values/jhove:value", NAMESPACES)
-    sequence = 1
-    nodes.each do |node|
-      mix = node.find_first("mix:mix", NAMESPACES)
-      bitstream = BitstreamObject.new
-      bitstream.uri = @fileObject.uri + "/" + sequence.to_s
-      compression = mix.find_first('mix:BasicDigitalObjectInformation/mix:Compression/mix:compressionScheme', NAMESPACES)
-      if (compression)
-        bitstream.formatName = compression.content
-      else 
-        bitstream.formatName = 'unknown'
-      end
-      bitstream.objectExtension = mix
-      @bitstreams << bitstream
-      sequence += 1
-    end
+   end
+   # traverse through multiple image bitstreams inside TIFF 
+   nodes = @jhove.find("//jhove:property[jhove:name/text()='NisoImageMetadata']/jhove:values/jhove:value", NAMESPACES)
+   sequence = 1
+   nodes.each do |node|
+     mix = node.find_first("mix:mix", NAMESPACES)
+     bitstream = BitstreamObject.new
+     bitstream.uri = @fileObject.uri + "/" + sequence.to_s
+     compression = mix.find_first('mix:BasicDigitalObjectInformation/mix:Compression/mix:compressionScheme', NAMESPACES)
+     if (compression)
+       bitstream.formatName = compression.content
+     else 
+       bitstream.formatName = 'unknown'
+     end
+     bitstream.objectExtension = mix
+     @bitstreams << bitstream
+     sequence += 1
+   end
   end
 
 end
