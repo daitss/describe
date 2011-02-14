b2a08a409e11b5934c721a4717ce7ab931f99b0e
Francesco Lazzarino <flazzarino@gmail.com>
2011-02-14 14:39:34 -0500

    modified for quite curl and using ps

--- memtest.rb
+++ memtest.rb
@@ -1,18 +1,18 @@
 #!/usr/local/env ruby
-require 'rubygems' 
+require 'rubygems'
 
 # d1 dedup report
 pid = ARGV.shift or raise "pid required"
 
-
-`curl http://localhost:7002/describe?location=file:///Users/Carol/tools/jhove/invalidMIX.tif`
+`curl http://localhost:7002/describe?location=file:///Users/franco/code/daitss/describe/files/tjpeg.tif`
 `curl http://localhost:7002/debug`
 puts `vmmap #{pid}`
-d = Dir.glob("/Users/Carol/testpackage/UF00053733_00004/*")
-d.each  do |file| 
-  puts "describe #{file}"
-  `curl http://localhost:7002/describe?location=file://#{file}`
+puts `ps u -p #{pid}`
+d = Dir.glob("/Users/franco/code/daitss/describe/UF00053733_00004/*")
+d.each  do |file|
+  `curl -s http://localhost:7002/describe?location=file://#{file}`
 end
 
 `curl http://localhost:7002/debug`
 puts `vmmap #{pid}`
+puts `ps u -p #{pid}`
