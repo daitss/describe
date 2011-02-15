786014490ea09cd684d7eb42d9858b8af7f85556
Carol Chou <cchou@ufl.edu>
2011-02-15 12:04:26 -0500

    add /proc for unix

--- memtest.rb
+++ memtest.rb
@@ -6,13 +6,18 @@ pid = ARGV.shift or raise "pid required"
 
 `curl http://localhost:7002/describe?location=file:///Users/franco/code/daitss/describe/files/tjpeg.tif`
 `curl http://localhost:7002/debug`
-puts `vmmap #{pid}`
+#puts `vmmap #{pid}`
 puts `ps u -p #{pid}`
+puts `cat /proc/#{pid}/status`
+puts `cat /proc/#{pid}/smaps`
+
 d = Dir.glob("/Users/franco/code/daitss/describe/UF00053733_00004/*")
 d.each  do |file|
   `curl -s http://localhost:7002/describe?location=file://#{file}`
 end
 
 `curl http://localhost:7002/debug`
-puts `vmmap #{pid}`
+#puts `vmmap #{pid}`
 puts `ps u -p #{pid}`
+puts `cat /proc/#{pid}/status`
+puts `cat /proc/#{pid}/smaps`
