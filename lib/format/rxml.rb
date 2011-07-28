1bf3aec66ae72be037837043cdcf6745e37dbf07
Carol Chou <cchou@ufl.edu>
2011-07-28 09:43:50 -0400

    log trimming

--- lib/format/rxml.rb
+++ lib/format/rxml.rb
@@ -7,7 +7,6 @@ class RXML < FormatBase
     # retrieve and dump the XML metadata
     xmlMD = @jhove.find_first('//jhove:property[jhove:name/text()="XMLMetadata"]', NAMESPACES)
     unless (xmlMD.nil?)
-      DescribeLogger.instance.info "transforming JHOVE output to XML"
       @fileObject.objectExtension = apply_xsl("xml2TextMD.xsl").root
     else 
       DescribeLogger.instance.warn "no XMLMetadata found"
