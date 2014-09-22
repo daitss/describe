ec748a4f115d49ff08a4f0fab1b165b2e6e608cd
Jon <jpitts@flvc.org>
2014-09-22 11:39:14 -0400

    Ruby 2.0.0 initial.

--- features/support/env.rb
+++ features/support/env.rb
@@ -2,12 +2,19 @@ require "rubygems"
 require "bundler"
 Bundler.setup
 require 'rack/test'
+require 'rspec'
 require 'rspec/expectations'
 
 $LOAD_PATH.unshift File.join(File.dirname(__FILE__), '..', '..', 'lib')
 require File.dirname(__FILE__) + "/../../app"
 
 Sinatra::Application.set :environment, :test
+
+RSpec.configure do |conf|
+  conf.expect_with :rspec do |c|
+    c.syntax = [:should, :expect]
+  end
+end
 
 World do
 
